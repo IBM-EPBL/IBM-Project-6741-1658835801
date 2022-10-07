f71ee88b9a34e4b39fd919e59900d6aaecb9f4b4
Kushal Darira <73783944+darirakushal@users.noreply.github.com>
2022-10-07 22:48:19 +0530

    Delete Balaji R Assignment 2.py

--- Balaji R Assignment 2.py
+++
@@ -1,9 +0,0 @@
-import random
-t=random.randint(1,100)
-h=random.randint(1,100)
-print("Temperature =",t);
-print("humidity =",h);
-if t > 50:
-    print("Alert : Temparature is Greater than 50 , Stay Safe")
-if h > 60:
-    print("Alert : Humidity Is greater Than 60 , Stay Safe")
