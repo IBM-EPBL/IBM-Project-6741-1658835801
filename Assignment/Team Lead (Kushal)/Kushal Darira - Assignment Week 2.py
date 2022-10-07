9d6adda3c4cbc5904b26036a290176bb82d557c3
Kushal Darira <73783944+darirakushal@users.noreply.github.com>
2022-10-07 13:44:22 +0530

    Add files via upload

---
+++ Assignment/Team Lead (Kushal)/Kushal Darira - Assignment Week 2.py
@@ -0,0 +1,9 @@
+import random
+flag=0
+temp=random.randint(1, 100)
+humid=random.randint(1, 100)
+if(temp>50):
+        print("temprature,humidity:",temp,humid)
+else:
+        print("High temprature",temp,humid)
+flag=1
